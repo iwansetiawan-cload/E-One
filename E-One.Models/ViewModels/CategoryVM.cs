185047278554aded67399754dbd618ed935c05bc
iwan setiawan <iwan.s@rds.co.id>
2022-06-26 13:01:06 +0700

    add paging info

--- E-One.Models/ViewModels/CategoryVM.cs
+++ E-One.Models/ViewModels/CategoryVM.cs
@@ -6,6 +6,7 @@ namespace E_One.Models.ViewModels
 {
     public class CategoryVM
     {
-        public IEnumerable<Category> Categories { get; set; }        
+        public IEnumerable<Category> Categories { get; set; }
+        public PagingInfo PagingInfo { get; set; }
     }
 }
